b37da3132b32fd630fce2dab4ce69ba452d53a5f
Davison Cruz <davison.cruz@outlook.com.br>
2026-08-06 23:04:00 -0300

    fix: adiciona banco.db ao gitignore e remove do rastreamento

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 const express = require("express");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
 const app = express();
 const PORTA = 3000;
 
 app.use(express.json());
 
+let db;
+
+async function inicializarBanco() {
+  db = await open({
+    filename: "./banco.db",
+    driver: sqlite3.Database,
+  });
+
+  await db.exec(`
+    Create TABLE IF NOT EXISTS gatos(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        idade INTEGER,
+        cor TEXT
+        status_saude TEXT
+        adotado BOLEAN DEFAULT 0
+        )
+    `);
+
+  console.log("📦 Banco de dados conectado e tabela criada!");
+}
+
 app.get("/", (req, res) => {
   res.json({
-    status: "Sucesso",
+    status: "Sucesso!",
     mensagem:
-      "Bem-vindo ao CatRescue API! Sistema pronto para receber a Lua e outros gatinhos.",
+      "Bem-vindo à CatRescue API! Sistema pronto para receber a Lua e outros felinos.",
   });
 });
 
-app.listen(PORTA, () => {
-  console.log(`🚀 Servidor rodando lindamente na porta ${PORTA}`);
+inicializarBanco().then(() => {
+  app.listen(PORTA, () => {
+    console.log(`🚀 Servidor rodando lindamente na porta ${PORTA}`);
+  });
 });
